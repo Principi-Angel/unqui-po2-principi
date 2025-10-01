ffc4729690718c0b30c4fa0511491a71da9e69b8
Principi-Angel <angel.principi@gmail.com>
2025-10-01 01:40:06 -0300

    <>

---
+++ test/ar/edu/unq/po2/tp6/clienteEMailTest/CorreoTest.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.po2.tp6.clienteEMailTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tp6.refactorClienteEmail.ClienteEMail;
+import ar.edu.unq.po2.tp6.refactorClienteEmail.Correo;
+
+class CorreoTest {
+	Correo correo;
+	ClienteEMail destinatario;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		destinatario = new ClienteEMail("desti", "1234");
+		correo = new Correo("asunto", destinatario, "hola");
+	}
+
+	@Test
+	void test() {
+		assertEquals(destinatario, correo.getDestinatario());
+	}
+
+}
