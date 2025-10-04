d7098600d5ca83954caf136cd3d0b864270c922b
Principi-Angel <angel.principi@gmail.com>
2025-10-04 01:09:43 -0300

    <>

---
+++ test/ar/edu/unq/po2/tp7/PokerStatusTest.java
@@ -0,0 +1,27 @@
+package ar.edu.unq.po2.tp7;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class PokerStatusTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
